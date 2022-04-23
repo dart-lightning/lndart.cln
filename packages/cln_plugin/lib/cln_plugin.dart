3478d79e12475cbedb3c32d2efb08e9187332e27
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-23 19:37:06 +0200

    plugin: add plugin package
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ packages/cln_plugin/lib/cln_plugin.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library cln_plugin;
+
+export 'src/cln_plugin_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
