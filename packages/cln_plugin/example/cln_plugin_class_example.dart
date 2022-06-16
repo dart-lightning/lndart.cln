a93c1735e59913dcc0c189e3520895444619e478
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-16 20:30:45 +0100

    plugin: implementing the example with the class extension.
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ packages/cln_plugin/example/cln_plugin_class_example.dart
@@ -0,0 +1,38 @@
+import 'package:cln_plugin/cln_plugin.dart';
+
+/// N.B. This style of developing a plugin is the suggested one
+/// for middle and big plugin.
+///
+/// This involve the possibility to develop in a more OOP style the
+/// code, but also to keep the code clean a simple to read.
+
+class MyPlugin extends Plugin {
+  Future<Map<String, Object>> firstMethod(
+      Plugin plugin, Map<String, Object> request) {
+    return Future.value({
+      "foo_opt": getOpt(key: "foo_opt"),
+    });
+  }
+
+  @override
+  void configurePlugin() {
+    // This is the standard way to register a plugin
+    registerOption(
+        name: "foo_opt",
+        type: "string",
+        def: "hello",
+        description: "This is an example of how the option looks like");
+
+    /// This is the standard way to register a custom rpc method
+    registerRPCMethod(
+        name: "first_method",
+        usage: "",
+        description: "This is an example of custom rpc method",
+        callback: (plugin, request) => firstMethod(plugin, request));
+  }
+}
+
+void main() {
+  var plugin = MyPlugin();
+  plugin.start();
+}
