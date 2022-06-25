11d12dc9df5b73680acc352501821c1447d43f74
Swapnil <swapnil06.st@gmail.com>
2022-06-26 00:46:57 +0530

    plugin: rpccommand refactoring.

--- packages/cln_plugin/lib/src/rpc_method/builtin/get_manifest.dart
+++ packages/cln_plugin/lib/src/rpc_method/builtin/get_manifest.dart
@@ -1,10 +1,10 @@
 import 'package:cln_plugin/cln_plugin.dart';
-import 'package:cln_plugin/src/rpc_method/rpc_command.dart';
+import 'package:cln_plugin/src/rpc_method/types/rpc_type.dart';
 
 /// GetManifest is a command implementation of the get manifest method
 /// that takes in input the callback to return the necessary data
 /// that core lightning required.
-class GetManifest extends RPCCommand {
+class GetManifest extends RPCMethod {
   GetManifest(
       {required Future<Map<String, Object>> Function(
               Plugin, Map<String, Object>)
